2d1b41bf8c4e4e70c076045a7e4372c038f0a4d6
jennyk1989 <jennifer0keene@gmail.com>
2021-12-15 19:49:35 -0600

    added high scores container

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,26 @@
 //variables
+const quizRules = $("#quiz-rules");
+const quizBox = $("#quiz-box");
+const scoreBox = $("#score-box");
+const startQuiz = $("#start-quiz");
+const highScores = $("#high-scores-box");
 
 //================ Start of Quiz ================
+//quiz box & score box hidden
+
 
 
 //================ Quiz Event ================
+//timer countdown starts
+
+//quiz box shows & quiz rules & score box still hidden
+
+
+//questions
 
 
 //================ End of Quiz ================
-
+//quiz box hideen & score box appears
 
 //================ Display High Scores ================
 
