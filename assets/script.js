7d0c2e474629ccb5173465f54c2f49bad565ca66
jennyk1989 <jennifer0keene@gmail.com>
2021-12-15 19:26:03 -0600

    js pseudocode

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//variables
+
+//================ Start of Quiz ================
+
+
+//================ Quiz Event ================
+
+
+//================ End of Quiz ================
+
+
+//================ Display High Scores ================
+
+
+//================ Event Listeners ================
+
+//click start button --> quiz event happens
+
